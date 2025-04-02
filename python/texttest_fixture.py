20bf5f99be12b4781a3df7f22cac95e3f312e336
saurav j <saurav.saurav12@gmail.com>
2025-04-02 10:41:05 +0530

    fixed the code in texttest_fixture

--- python/texttest_fixture.py
+++ python/texttest_fixture.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from gilded_rose import *
-
+from gilded_rose import GildedRose
+from product_list import ProductList
 if __name__ == "__main__":
     print ("OMGHAI!")
-    items = [
-             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name="Aged Brie", sell_in=2, quality=0),
-             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-             Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
-            ]
+    items = [GildedRose.create_item(name="+5 Dexterity Vest",sell_in= 10,quality= 20),
+         GildedRose.create_item(name="Aged Brie", sell_in=2, quality=0),
+         GildedRose.create_item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+         GildedRose.create_item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+         GildedRose.create_item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+         GildedRose.create_item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+         GildedRose.create_item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+         GildedRose.create_item(name="Conjured Mana Cake", sell_in=3, quality=6)
+         ]
 
     days = 2
     import sys
@@ -27,4 +25,6 @@ if __name__ == "__main__":
         for item in items:
             print(item)
         print("")
-        GildedRose(items).update_quality()
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        # GildedRose(items).update_quality()
