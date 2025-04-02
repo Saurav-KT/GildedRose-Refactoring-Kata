daa92b11766ce241cff6e02bde4c58b34dd5719a
saurav j <saurav.saurav12@gmail.com>
2025-04-02 10:25:24 +0530

    added edge cases for quality boundaries

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -2,7 +2,6 @@
 import unittest
 
 from gilded_rose import GildedRose
-from item import Item, RegularItem, AgedBrie, BackstagePass, Sulfuras
 from product_list import ProductList
 
 
@@ -34,6 +33,26 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(-1, items[0].sell_in)
         self.assertEqual(12, items[0].quality)
+
+    def test_backstage_pass_long_before_sell_date(self):
+        items = [GildedRose.create_item(ProductList.BACK_STAGE_PASSES, 15, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(14, items[0].sell_in)
+        self.assertEqual(11, items[0].quality)
+
+    def test_backstage_pass_after_sell_date(self):
+        items = [GildedRose.create_item(ProductList.BACK_STAGE_PASSES, 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(0, items[0].quality)
+
+    def test_backstage_pass_quality_never_exceeds_50(self):
+        items = [GildedRose.create_item(ProductList.BACK_STAGE_PASSES, 5, 49)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(50, items[0].quality)
 
 
 
